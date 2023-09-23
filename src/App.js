c1b1683dfc8a2fdd4fbe2378e70a0f4ac86d1b1e
Tyler Cagle <tylerjcagle@gmail.com>
2023-09-23 18:40:28 -0400

    update

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import { useRoute, useLocation } from 'wouter'
 import { easing } from 'maath'
 import getUuid from 'uuid-by-string'
 
-const GOLDENRATIO = 1.61803398875
+const GOLDENRATIO = 1.44444444444444
 
 export const App = ({ images }) => (
-  <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
+  <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }} toneMapped={false} >
     <color attach="background" args={['#050505']} />
     <fog attach="fog" args={['#050505', 0, 15]} />
     <group position={[0, -0.5, 0]}>
@@ -43,7 +43,7 @@ function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() })
     clicked.current = ref.current.getObjectByName(params?.id)
     if (clicked.current) {
       clicked.current.parent.updateWorldMatrix(true, true)
-      clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 2))
+      clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1))
       clicked.current.parent.getWorldQuaternion(q)
     } else {
       p.set(0, 0, 5.5)
@@ -65,28 +65,23 @@ function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() })
 }
 
 function Frame({ url, c = new THREE.Color(), ...props }) {
-  const image = useRef()
-  const frame = useRef()
-  const [, params] = useRoute('/item/:id')
   const [hovered, hover] = useState(false)
-  const [rnd] = useState(() => Math.random())
   const name = getUuid(url)
-  const isActive = params?.id === name
-  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/drei.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
+  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/vid.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
+  useEffect(() => void video.play(), [video])
   useCursor(hovered)
-  useFrame((state, dt) => {
-    image.current.material.zoom = 2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
-    easing.damp3(image.current.scale, [0.85 * (!isActive && hovered ? 0.85 : 1), 0.9 * (!isActive && hovered ? 0.905 : 1), 1], 0.1, dt)
-  })
   return (
     <group {...props}>
-      <mesh name={name} scale={[1, GOLDENRATIO, 0.05]} position={[0, GOLDENRATIO / 2, 0]}>
+      <mesh name={name} scale={[GOLDENRATIO, 1, 0.05]} position={[0, GOLDENRATIO / 2, 0]}>
         <boxGeometry />
         <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
-        <Image raycast={() => null} ref={image} position={[0, 0, 10]} url={url} />
+        <meshBasicMaterial toneMapped={false}>
+          <videoTexture attach="map" args={[video]} />
+        </meshBasicMaterial>
       </mesh>
-      <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.65, GOLDENRATIO, 0]} fontSize={0.025}>
-        {name.split('-').join(' ')}
+      <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[GOLDENRATIO / 2 + 0.05, 0.31, 0]} fontSize={0.025}>
+        Off–Brand
+        SUPLABS®
       </Text>
     </group>
   )
